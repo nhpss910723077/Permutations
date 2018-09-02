e2e39361c6c88a1fe263ff78bc7e299c0581e004
nhpss910723077 <nhpss91064@gmail.com>
2018-09-02 20:36:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+﻿var question = [];
+var ans = "";
+var tem = "";
+var count = 0;
+
+function getQuestion() {
+    for (var i = 0; i < $('.input-question').val().length; i++) {
+        question[i] = $('.input-question').val()[i];
+    }
+}
+
+function permutations(length) {
+    if (length === 1) {
+        for (var i = 0; i < question.length; i++) {
+            ans += question[i];
+        }
+        $(".answer").append("<p>" + ans + "</p>")
+        console.log(ans);
+        ans = "";
+        count++;
+    } else {
+        for (var i = 0; i < length; i++) {
+            permutations(length - 1);
+
+            tem = question[question.length - length];
+            for (var j = question.length - length; j < question.length - 1; j++) {
+                question[j] = question[j + 1];
+            }
+            question[question.length - 1] = tem;
+        }
+    }
+}
+
+$('.btn-submit').click(function () {
+    question = [];
+    getQuestion();
+    permutations(question.length);
+    console.log(count);    
+});
+
+$('.btn-reset').click(function () {
+    $('.answer').html("");
+    count = 0;
+    question = [];
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
